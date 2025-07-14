a46de0d2ac73a9699629adb73cc18c08b4379aa7
Daniel Kempner <daniel.kempner@instacart.com>
2025-07-14 10:41:43 -0700

    bun doesnt work with pm2 when > 1 instance

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "index.js",
+      interpreter: "bun",
+      //   exec_mode: "cluster",
+      //   instances: 2,
+    },
+  ],
+};
